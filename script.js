3d532158b2db7d0437ac95064451f50d94068e20
10Bitz <91356027+10Bitz@users.noreply.github.com>
2023-07-28 16:55:40 -0230

    Update script.js

--- script.js
+++ script.js
@@ -1452,6 +1452,21 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+var coll = document.getElementsByClassName("collapsiblePersonal");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 
 // Add click event listener to skill1Select dropdown
 skill1SelectElement.addEventListener('click', function() {
